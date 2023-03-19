24898eb7c9ecb87e7f9f5530abf1996de8dfda39
Shahan <aliyevsahan@gmail.com>
2023-03-19 12:49:21 +0400

    Wallet model has been created

--- Core/admin.py
+++ Core/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import PhonePrefix, WareHouse, Currency
+from .models import PhonePrefix, WareHouse, Currency, Wallet
 
 admin.site.register(PhonePrefix)
 admin.site.register(WareHouse)
 admin.site.register(Currency)
+admin.site.register(Wallet)
--- Core/models.py
+++ Core/models.py
@@ -16,4 +16,18 @@ class Currency(models.Model):
 
     def __str__(self):
         return f'{self.name} {self.id}'
-    
+
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Wallet(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    currency = models.ForeignKey(Currency, on_delete=models.CASCADE)
+    balance = models.DecimalField(max_digits=5, decimal_places=2, default=0)
+
+    def __str__(self):
+        return f"{self.user.first_name}'s {self.currency.name} balance {self.balance}"
