3d60f3906337302b4747681e64fe3f3f2e0b33cb
Shahan <aliyevsahan@gmail.com>
2023-04-02 11:17:17 +0400

    Unrelated code was deleted

--- core/models.py
+++ core/models.py
@@ -105,9 +105,6 @@ class News(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    class Meta:
-
-        verbose_name_plural = "News"
 
     def save(self, *args, **kwargs):
         if not self.slug:
