da9d760f9711c254940778e4c37e40998b3c9f7b
Shahan <aliyevsahan@gmail.com>
2023-04-04 14:46:14 +0400

    Debug codes were removed

--- order/models.py
+++ order/models.py
@@ -8,8 +8,4 @@ class Status(models.Model):
     order = models.PositiveSmallIntegerField(default=0)
 
     def __str__(self):
-        reveal_type(self.id) 
-        reveal_type(self.name)
-        reveal_type(self.next_status)
-        reveal_type(self.order)
-        return self.id
+        return self.name
