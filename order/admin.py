0726b007c485abc75b4d8853bbb34e971fbf8928
Shahan <aliyevsahan@gmail.com>
2023-04-10 18:34:29 +0400

    Declaration model was crated, status history and limit checking are remaining

--- order/admin.py
+++ order/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from order.models import Status
+from order.models import Status, Declaration
 
 admin.site.register(Status)
 
-# Register your models here.
+
+@admin.register(Declaration)
+class DeclarationAdmin(admin.ModelAdmin):
+
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        super().save_model(request, obj, form, change)
