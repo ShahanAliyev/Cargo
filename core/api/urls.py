d67c8ed283e96f1756b335b285c17719b9f5a331
Shahan <aliyevsahan@gmail.com>
2023-05-06 15:34:20 +0400

    Endpoint was modified

--- core/api/urls.py
+++ core/api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from core.api.views import get_warehouse, get_warehouses
 
 urlpatterns = [
-    path('warehouses', get_warehouses, name="warehouses" ),
+    path('warehouses/', get_warehouses, name="warehouses" ),
     path('warehouses/<int:warehouse_id>/', get_warehouse, name="warehouse"),
 ]
